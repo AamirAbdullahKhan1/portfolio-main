cf2ce2e9f4e47cd707239d2501717305a4a03968
Aamir Khan <sengoku264@gmail.com>
2024-12-24 07:50:58 +0530

    ft projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,6 +2,7 @@ import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
 import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
 import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
 import CheckIcon from '@/assets/icons/check-circle.svg'
+import ArrowUpRightIcon from '@/assets/icons/arrow-up-right.svg'
 import Image from "next/image";
 
 const portfolioProjects = [
@@ -53,29 +54,33 @@ export const ProjectsSection = () => {
         </div>
         <h2 className="text-center font-serif mt-6 text-3xl">Featured Projects</h2>
         <p className="text-center mt-4 text-white/60">See how I transformed concepts into engaging digital experiences</p>
-        <div className="flex flex-col mt-10 ">
+        <div className="flex flex-col mt-10 gap-20 ">
           {portfolioProjects.map(project => (
-            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 p-8">
-              <div className="flex">
-                <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
-                  <span>{project.company}</span>
-                  <span>&bull;</span>
-                  <span>{project.year}</span>
-                </div>
+            <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0 overflow-hidden after:z-10 after:content-[''] after:absolute after:inset-0 after:outline-2 after:outline after:-outline-offset-2 after:rounded-3xl after:outline-white/20 px-8 pt-8">
+              
+              <div className="bg-gradient-to-r from-emerald-300 to-sky-400 inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
+                <span>{project.company}</span>
+                <span>&bull;</span>
+                <span>{project.year}</span>
               </div>
+              
               <h3 className="font-serif text-2xl mt-2">{project.title}</h3>
               <hr className="border-t-2 border-white/5 mt-4"/>
-              <ul>
+              <ul className="flex flex-col gap-4 mt-4 ">
                 {project.results.map(result => (
-                  <li>
+                  <li className="flex gap-2 text-sm text-white/50">
+                    <CheckIcon className = "size-5"/>
                     <span>{result.title}</span>
                   </li>
                 ))}
               </ul>
               <a href={project.link}>
-                <button>View Live Site</button>
+                <button className="bg-white text-gray-900 h-12 w-full rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8">
+                  <span>Visit Live Site</span>
+                  <ArrowUpRightIcon className = "size-4"/>
+                </button>
               </a>
-              <Image src={project.image} alt={project.title}/>
+              <Image src={project.image} alt={project.title} className="mt-8 -mb-4"/>
             </div>
           ))}
         </div>
